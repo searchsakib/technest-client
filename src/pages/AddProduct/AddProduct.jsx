fff1af1c09b0668f82c454a0177a0ccd078da49d
searchsakib <searchsakib@gmail.com>
2023-10-18 20:13:19 +0600

    basic add product form added

--- src/pages/AddProduct/AddProduct.jsx
+++ src/pages/AddProduct/AddProduct.jsx
@@ -1,7 +1,148 @@
 const AddProduct = () => {
+  const handleAddProduct = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const name = form.name.value;
+    const brand = form.brand.value;
+    const type = form.type.value;
+    const price = form.price.value;
+    const description = form.description.value;
+    const rating = form.rating.value;
+    const image = form.image.value;
+
+    const newProduct = {
+      name,
+      brand,
+      type,
+      price,
+      description,
+      rating,
+      image,
+    };
+    console.log(newProduct);
+  };
   return (
-    <div>
-      <h2>I am Add Product</h2>
+    <div className="bg-[#F4F3F0] p-16 mb-[100px]">
+      <h2 className="text-3xl font-extrabold mb-12 text-[#3B3B3B] text-center">
+        Add Product
+      </h2>
+      <div>
+        <div className="mx-auto max-w-xl">
+          <form onSubmit={handleAddProduct} className="space-y-5">
+            <div className="grid grid-cols-12 gap-8">
+              <div className="col-span-6">
+                <label
+                  htmlFor="example7"
+                  className="mb-1 block text-sm font-medium text-gray-700"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500"
+                  placeholder="Name"
+                  name="name"
+                />
+              </div>
+              <div className="col-span-6">
+                <label
+                  htmlFor="example8"
+                  className="mb-1 block text-sm font-medium text-gray-700"
+                >
+                  Brand Name
+                </label>
+                <input
+                  type="text"
+                  className="bloc p-2 w-full rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500"
+                  placeholder="Brand Name"
+                  name="brand"
+                />
+              </div>
+              <div className="col-span-6">
+                <label
+                  htmlFor="example7"
+                  className="mb-1 block text-sm font-medium text-gray-700"
+                >
+                  Type
+                </label>
+                <input
+                  type="text"
+                  className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500"
+                  placeholder="Type"
+                  name="type"
+                />
+              </div>
+              <div className="col-span-6">
+                <label
+                  htmlFor="example7"
+                  className="mb-1 block text-sm font-medium text-gray-700"
+                >
+                  Price
+                </label>
+                <input
+                  type="number"
+                  className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500"
+                  placeholder="Price"
+                  name="price"
+                />
+              </div>
+              <div className="col-span-6">
+                <label
+                  htmlFor="example7"
+                  className="mb-1 block text-sm font-medium text-gray-700"
+                >
+                  Short Description
+                </label>
+                <input
+                  type="text"
+                  className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500"
+                  placeholder="Short Description"
+                  name="description"
+                />
+              </div>
+              <div className="col-span-6">
+                <label
+                  htmlFor="example7"
+                  className="mb-1 block text-sm font-medium text-gray-700"
+                >
+                  Rating
+                </label>
+                <input
+                  type="number"
+                  className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500"
+                  placeholder="Rating"
+                  name="rating"
+                  min="1"
+                  max="5"
+                />
+              </div>
+              <div className="col-span-12">
+                <label
+                  htmlFor="example7"
+                  className="mb-1 block text-sm font-medium text-gray-700"
+                >
+                  Image
+                </label>
+                <input
+                  type="text"
+                  className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500"
+                  placeholder="Image URL"
+                  name="image"
+                />
+              </div>
+              <div className="col-span-12">
+                <input
+                  type="submit"
+                  className="btn btn-block hover:bg-white hover:text-black bg-[#3B3B3B] text-white"
+                  value="Add Product"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
