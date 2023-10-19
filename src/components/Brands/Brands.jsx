65d2e76dc9e87f84d7454af86314f0645d85bff8
searchsakib <searchsakib@gmail.com>
2023-10-19 07:03:21 +0600

    brands.json added

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,7 +1,19 @@
 const Brands = () => {
   return (
-    <div className="max-w-[1320px] mx-auto rounded-2xl bg-green-200 h-[576px] mt-[100px] mb-[100px]">
-      <h2 className="text-4xl text-center pt-16">This is Brands</h2>
+    <div className="max-w-[1320px] mx-auto rounded-2xl bg-gray-100  mt-[100px] mb-[100px] p-10">
+      I am brands
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-10">
+        <div className="bg-orange-100 ">
+          <div className="max-w-[150px] mx-auto">
+            <img
+              src="/public/images/apple.png"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </div>
+          <h2 className="text-center text-3xl pb-5 ">Apple</h2>
+        </div>
+      </div> */}
     </div>
   );
 };
