e3322543b1ce347ef70fb70e91c95fab2c359cf3
searchsakib <searchsakib@gmail.com>
2023-10-19 08:02:41 +0600

    BrandCard with responsive done

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,19 +1,28 @@
+import { useEffect, useState } from 'react';
+import BrandCard from '../BrandCard/BrandCard';
+
 const Brands = () => {
+  const [myBrands, setMyBrands] = useState([]);
+  useEffect(() => {
+    fetch('/brands.json')
+      .then((res) => res.json())
+      .then((data) => setMyBrands(data));
+  }, []);
+
   return (
-    <div className="max-w-[1320px] mx-auto rounded-2xl bg-gray-100  mt-[100px] mb-[100px] p-10">
-      I am brands
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-10">
-        <div className="bg-orange-100 ">
-          <div className="max-w-[150px] mx-auto">
-            <img
-              src="/public/images/apple.png"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </div>
-          <h2 className="text-center text-3xl pb-5 ">Apple</h2>
-        </div>
-      </div> */}
+    <div className="max-w-[1320px] mx-auto rounded-2xl bg-orange-100 mt-[100px] mb-[100px] py-[70px] px-7 md:px-16 lg:px-20">
+      <div className="max-w-3xl mx-auto text-center pb-10">
+        <h2 className="text-3xl font-extrabold sm:text-4xl">Renowned Brands</h2>
+        <p className="mt-4 text-lg text-gray-700">
+          We sell top-tier products from industry giants, redefining innovation
+          and reliability for our customers
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-10">
+        {myBrands?.map((myBrand, idx) => (
+          <BrandCard key={idx} myBrand={myBrand}></BrandCard>
+        ))}
+      </div>
     </div>
   );
 };
