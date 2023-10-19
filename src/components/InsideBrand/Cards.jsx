fd21a017d0b0fbf16c10caa3f6ff39cbeb7974b3
searchsakib <searchsakib@gmail.com>
2023-10-19 17:40:42 +0600

    at last filter worked for brands

--- src/components/InsideBrand/Cards.jsx
+++ src/components/InsideBrand/Cards.jsx
@@ -1,9 +1,22 @@
 const Cards = ({ myProduct }) => {
-  const { name } = myProduct || {};
+  const { name, brand, type, price, description, rating, image } =
+    myProduct || {};
 
   return (
     <div className="py-8">
-      <h3 className="bg-green-200 p-20"> {name} </h3>
+      <div className="bg-green-200 p-4">
+        <img
+          src={image}
+          alt={name}
+          className="w-40 h-40 object-cover mx-auto"
+        />
+        <h3 className="text-xl font-semibold text-center mt-4">{name}</h3>
+        <p className="text-center text-gray-600 capitalize">Brand: {brand}</p>
+        <p className="text-center text-gray-600">Type: {type}</p>
+        <p className="text-center text-gray-600">Price: {price}</p>
+        <p className="text-center text-gray-600">Description: {description}</p>
+        <p className="text-center text-gray-600">Rating: {rating}</p>
+      </div>
     </div>
   );
 };
--- src/components/InsideBrand/InsideBrandDetails.jsx
+++ src/components/InsideBrand/InsideBrandDetails.jsx
@@ -2,31 +2,38 @@ import { useEffect, useState } from 'react';
 import Cards from './Cards';
 
 const InsideBrandDetails = ({ insideBrand }) => {
-  const { brand_image, brand_name } = insideBrand || {};
+  const { brand_name } = insideBrand || {};
 
   const [myProducts, setMyProducts] = useState([]);
+  const [theProduct, setTheProduct] = useState([]);
+
   useEffect(() => {
     fetch('https://technest-server.vercel.app/products')
       .then((res) => res.json())
-      .then((data) => setMyProducts(data));
+      .then((data) => {
+        console.log('Fetched data:', data); // Check if data is fetched correctly
+        setMyProducts(data);
+      })
+      .catch((error) => console.error('Error fetching data:', error)); // Check for any errors during the fetch
   }, []);
+
+  useEffect(() => {
+    if (myProducts && myProducts.length > 0) {
+      const findProduct = myProducts.filter(
+        (prod) => prod.brand === brand_name
+      );
+      console.log('Filtered products:', findProduct); // Check the filtered products
+      setTheProduct(findProduct);
+    }
+  }, [myProducts, brand_name]);
 
   return (
     <div className="max-w-[1320px] mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-10">
-        {myProducts?.map((myProduct, idx) => (
-          <Cards key={idx} myProduct={myProduct}></Cards>
+        {theProduct.map((product) => (
+          <Cards key={product._id} myProduct={product} />
         ))}
       </div>
-
-      {/* <div className="text-center">My products length {myProducts.length} </div>
-      <div className="mx-auto w-[150px]">
-        <img src={brand_image} alt="" />
-      </div>
-      <h2 className="text-3xl text-orange-500 text-center pb-20">
-        {' '}
-        {brand_name}{' '}
-      </h2> */}
     </div>
   );
 };
